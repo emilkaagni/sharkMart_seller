42edc30d4e70ca3c8868e8fc272f1fa16170352c
emilkaagni <basnyatemil02@gmail.com>
2023-07-24 15:12:53 +0545

    Products screens ui updated

---
+++ lib/const/lists.dart
@@ -0,0 +1,7 @@
+import 'package:Indi_seller/const/const.dart';
+
+const profileIconsList = [icShopSettings, icChat, icAccount];
+const profileIconsTitle = [shopSettings, messages, logout];
+
+const popupMenuTitles = [featured, edit, remove];
+const popupMenuIcons  = [Icons.featured_play_list, Icons.edit, Icons.delete];
--- lib/views/products_screen/products_screen.dart
+++ lib/views/products_screen/products_screen.dart
@@ -28,6 +28,25 @@ class ProductsScreen extends StatelessWidget {
               leading: Image.asset(imgProduct, width: 100, height: 100, fit: BoxFit.cover),
               title: boldText( text: "Product title", color: fontGrey),
               subtitle: normalText(text: "\$40", color: darkGrey),
+              trailing: VxPopupMenu(
+                arrowSize: 0.0,
+                  menuBuilder: ()=>Column(
+                children: List.generate(
+                    popupMenuTitles.length, 
+                        (index) => Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Row(
+                            children: [
+                              Icon(popupMenuIcons[index]),
+                              5.widthBox,
+                              normalText(text: popupMenuTitles[index], color: darkGrey)
+                  ],
+                ).onTap(() { }),
+                        ),
+                ),
+              ).box.white.rounded.width(200).make(),
+                  clickType: VxClickType.singleClick,
+              child: const Icon(Icons.more_vert_rounded)),
             ))
           ),
         )),
