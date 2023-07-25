74821f18150c0a29a566e8181e25a2bd94df75f4
emilkaagni <basnyatemil02@gmail.com>
2023-07-25 14:04:23 +0545

    connected messages to the fire base now it shows senders name, preview of the chat and the time when last message was sent

--- lib/services/store_services.dart
+++ lib/services/store_services.dart
@@ -4,4 +4,7 @@ class StoreServices{
   static getProfile(uid){
     return firestore.collection(vendorsCollection).where('id', isEqualTo: uid).get();
   }
+  static getMessages(uid){
+    return firestore.collection(chatsCollection).where('toId', isEqualTo: uid).snapshots();
+  }
 }
--- lib/views/messages_screen/messages_screen.dart
+++ lib/views/messages_screen/messages_screen.dart
@@ -1,8 +1,13 @@
 import 'package:Indi_seller/const/colors.dart';
+import 'package:Indi_seller/const/const.dart';
+import 'package:Indi_seller/services/store_services.dart';
 import 'package:Indi_seller/views/messages_screen/chat_screen.dart';
+import 'package:Indi_seller/views/widgets/loading_indicator.dart';
 import 'package:Indi_seller/views/widgets/text_style.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart'as intl;
 
 import '../../const/strings.dart';
 
@@ -21,31 +26,43 @@ class MessagesScreen extends StatelessWidget {
         ),
         title: boldText( text: messages, size: 16.0, color: fontGrey),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Column(
-            children: List.generate(
-              20,
-                (index)=>ListTile(
-                  onTap: (){
-                    Get.to(()=>const ChatScreen());
-                  },
-                  leading: const CircleAvatar(
-                    backgroundColor: purpleColor,
-                    child: Icon(
-                      Icons.person,
-                      color: white,
-                    ),
-                  ),
-                  title: boldText(text: "Username", color: fontGrey),
-                  subtitle: normalText(text: "Last message...", color: darkGrey),
-                  trailing: normalText(text: "10:45 PM", color: darkGrey),
-                )
-            ),
-          ),
-        ),
+      body: StreamBuilder(
+        stream: StoreServices.getMessages(currentUser!.uid),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot>snapshot){
+          if(!snapshot.hasData){
+            return loadingIndicator();
+          }else{
+            var data =snapshot.data!.docs;
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  children: List.generate(data.length, (index){
+                    var t = data[index]['created_on']==null ? DateTime.now() : data[index]['created_on'].toDate();
+                    var time = intl.DateFormat("h:mma").format(t);
+
+                            return ListTile(
+                              onTap: (){
+                                Get.to(()=>const ChatScreen());
+                              },
+                              leading: const CircleAvatar(
+                                backgroundColor: purpleColor,
+                                child: Icon(
+                                  Icons.person,
+                                  color: white,
+                                ),
+                              ),
+                              title: boldText(text: "${data[index]['sender_name']}", color: fontGrey),
+                              subtitle: normalText(text: "${data[index]['last_msg']}", color: darkGrey),
+                              trailing: normalText(text: time, color: darkGrey),
+                            );
+                          }),
+                ),
+              ),
+            );
+          }
+        },
       ),
     );
   }
